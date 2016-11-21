4481ed99d5ba3998e4271f6c02a4ceed90affead
Daniel Probst <daenuprobst@outlook.com>
2016-11-21 21:33:59 +0100

    Implemented UI and socket based comm in a webworker.

---
+++ app/scripts/app.js
@@ -0,0 +1,112 @@
+(function() {
+    // Globals
+    var socket = null;
+    var lore = null;
+    var availableSets = null;
+    var currentSet = null;
+    var socketWorker = new Worker('scripts/socketWorker.js');
+
+    // Events
+    var fullscreenSwitch = document.getElementById('switch-fullscreen');
+    fullscreenSwitch.addEventListener('change', function() {
+        if(fullscreenSwitch.checked) {
+            launchIntoFullscreen(document.documentElement);
+        }
+        else {
+            exitFullscreen();
+        }
+    }, false);
+
+    var selectSet = document.getElementById('select-set');
+    var loaderSelectSet = document.getElementById('loader-select-set');
+
+    selectSet.addEventListener('change', function() {
+        socketWorker.postMessage({ cmd: 'load', message: selectSet.value });
+        selectSet.parentElement.style.pointerEvents = 'none';
+        show(loaderSelectSet);
+    }, false);
+
+    // UI - data
+    function populateServerSets() {
+        removeChildren(selectSet);
+        appendEmptyOption(selectSet);
+        for(var i = 0; i < availableSets.length; i++) {
+            currentSet = availableSets[i];
+            appendOption(selectSet, currentSet[0], currentSet[1]);
+        }
+    }
+
+    // Socket.IO communication
+    document.addEventListener("DOMContentLoaded", function(event) {
+        lore = Lore.init('lore', { clearColor: '#212121' });
+        
+        socketWorker.onmessage = function(e){
+            var cmd = e.data.cmd;
+            var message = e.data.message;
+
+            if(cmd === 'initresponse') {
+                availableSets = message;
+                populateServerSets();
+            }
+            else if(cmd === 'loadresponse') {
+                selectSet.parentElement.style.pointerEvents = 'auto';
+                document.getElementById('datatitle').innerHTML = currentSet[1];
+                hide(loaderSelectSet);
+                for(var i = 0; i < message.data.length; i++) message.data[i] = new Uint16Array(message.data[i])
+
+                var pointHelper = new Lore.PointHelper(lore, 'TestGeometry', 'default');
+                pointHelper.setPositionsXYZColor(message.data[0], message.data[1], message.data[2], new Lore.Color(0.1, 0.2, 0.8));
+            }
+        }; 
+    });
+
+    // Helpers
+    function launchIntoFullscreen(element) {
+        if(element.requestFullscreen) {
+            element.requestFullscreen();
+        } else if(element.mozRequestFullScreen) {
+            element.mozRequestFullScreen();
+        } else if(element.webkitRequestFullscreen) {
+            element.webkitRequestFullscreen();
+        } else if(element.msRequestFullscreen) {
+            element.msRequestFullscreen();
+        }
+    }
+
+    function exitFullscreen() {
+        if(document.exitFullscreen) {
+            document.exitFullscreen();
+        } else if(document.mozCancelFullScreen) {
+            document.mozCancelFullScreen();
+        } else if(document.webkitExitFullscreen) {
+            document.webkitExitFullscreen();
+        }
+    }
+
+    function removeChildren(element) {
+        while(element.firstChild) {
+            element.removeChild(element.firstChild);
+        }
+    }
+
+    function appendOption(element, value, text) {
+        var option = document.createElement('option');
+        option.value = value;
+        option.innerHTML = text;
+        element.appendChild(option);
+    }
+
+    function appendEmptyOption(element) {
+        var option = document.createElement('option');
+        element.appendChild(option);
+    }
+
+    function hide(element) {
+        element.classList.add('hidden');
+    }
+
+    function show(element) {
+        element.classList.remove('hidden');
+    }
+
+})();
---
+++ app/scripts/socketWorker.js
@@ -0,0 +1,38 @@
+importScripts('/libs/socketio/socket.io-1.4.5.js');
+var socket = socket = io.connect('http://46.101.234.147:8080/underdark');
+
+socket.on('initresponse', function(msg) {
+    postMessage({ cmd: 'initresponse', message: msg.data });
+});
+
+socket.on('loadresponse', function(msg) { 
+    var arr = toArray(msg.data);
+    for(var i = 0; i < arr.length; i++) arr[i] = arr[i].buffer;
+    postMessage({ cmd: 'loadresponse', message: { data: arr, data_types: msg.data_types } });
+});
+
+onmessage = function(e) {
+    var cmd = e.data.cmd;
+    var message = e.data.message;
+    
+    socket.emit(cmd, message);
+}
+
+function toArray(str) {
+    var lines = str.split('\n');
+    var arrays = [];
+
+    var nValues = lines[0].split(',').length;
+    for(var i = 0; i < nValues; i++) {
+        arrays.push(new Uint16Array(lines.length));
+    }
+
+    for(var i = 0; i < lines.length; i++) {
+        var values = lines[i].split(',');
+        for(var j = 0; j < values.length; j++) {
+            arrays[j][i] = parseInt(values[j]);
+        }
+    }
+
+    return arrays;
+}
