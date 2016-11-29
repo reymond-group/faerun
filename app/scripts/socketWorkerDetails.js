40eab57424cd83ee57fcc810cd7d885b3d4bc04e
Daniel Probst <daenuprobst@outlook.com>
2016-11-29 20:49:21 +0100

    config for demo

--- app/scripts/socketWorkerDetails.js
+++ app/scripts/socketWorkerDetails.js
@@ -1,7 +1,8 @@
 importScripts('/libs/socketio/socket.io-1.4.5.js');
 importScripts('/scripts/faerun-common.js');
 
-var socket = io.connect('http://130.92.75.77:8080/underdark');
+// var socket = io.connect('http://192.168.1.3:8080/underdark');
+var socket = socket = io.connect('http://46.101.234.147:8080/underdark');
 
 socket.on('initresponse', function (msg) {
   postMessage({
